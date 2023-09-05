9000bed096d92cae273091fd0381658b0e9f7889
andersons-tuche23 <134718682+andersons-tuche23@users.noreply.github.com>
2023-09-05 18:28:29 -0300

    organo-alura

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner(){
+    return(
+        <header className='banner'>
+        <img src="/banner.png" alt="o banner principal da página do organo" />
+        </header>
+    )
+
+}
+
+export default Banner
